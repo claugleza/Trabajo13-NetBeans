6360c2f27985d7bd51adb540ea78ca6e3d131a62
claud <claud@HP_CLAUDIA>
2024-04-16 23:50:11 +0200

    Version 1

--- src/calculador/CALCULADOR.java
+++ src/calculador/CALCULADOR.java
@@ -9,15 +9,19 @@ public class CALCULADOR {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner (System.in);
-        System.out.println("por favor ingrese dos nnumeros; ");
-        int num1= teclado.nextInt();
+        
+       System.out.println("por favor ingrese dos nnumeros; ");
+       //definimos numeros que seran introducidos por teclado 
+       int num1= teclado.nextInt();
         int num2 = teclado.nextInt();
         
+        //definimos las operaciones 
         int suma= num1+num2;
         int resta= num1-num2;
         int multiplicacion = num1*num2;
         int division = num1/num2;
         
+        //menu escrito para ver ls opciones que podremos elegir 
         System.out.println("seleccione la operacion que desa realizar : ");
         System.out.println("1. Suma ");
         System.out.println("2. Resta");
@@ -26,6 +30,7 @@ public class CALCULADOR {
         
         int choice= teclado.nextInt();
         
+        //swich para elegir la opcion que quermaos realizar 
         switch(choice){
             case 1:
                 System.out.println("el resultado de la suma es: "+suma);
