2819021aabb9a9d66fcd8af3c31629d89153870c
claud <claud@HP_CLAUDIA>
2024-04-16 23:55:25 +0200

    Version 2

--- src/calculador/CALCULADOR.java
+++ src/calculador/CALCULADOR.java
@@ -8,50 +8,44 @@ public class CALCULADOR {
 
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner (System.in);
+
+        Operaciones calcul = new Operaciones();
+	Scanner teclado = new Scanner(System.in);
+        System.out.println("Por favor ingrese dos números:");
         
-       System.out.println("por favor ingrese dos nnumeros; ");
-       //definimos numeros que seran introducidos por teclado 
-       int num1= teclado.nextInt();
+ 
+        int num1 = teclado.nextInt();
         int num2 = teclado.nextInt();
         
-        //definimos las operaciones 
-        int suma= num1+num2;
-        int resta= num1-num2;
-        int multiplicacion = num1*num2;
-        int division = num1/num2;
-        
-        //menu escrito para ver ls opciones que podremos elegir 
-        System.out.println("seleccione la operacion que desa realizar : ");
-        System.out.println("1. Suma ");
+
+        System.out.println("Seleccione la operación que desea realizar:");
+        System.out.println("1. Suma");
         System.out.println("2. Resta");
-        System.out.println("3. Multiplicaacion");
-        System.out.println("4. Division ");
+        System.out.println("3. Multiplicación");
+   	System.out.println("4. División");
         
-        int choice= teclado.nextInt();
-        
-        //swich para elegir la opcion que quermaos realizar 
-        switch(choice){
+        int choice = teclado.nextInt();
+
+ 
+        switch(choice) {
             case 1:
-                System.out.println("el resultado de la suma es: "+suma);
+                System.out.println("El resultado de la suma es: " +calcul.suma(num1, num2));
                 break;
-             
-            case 2: 
-                System.out.println("el resultado de la resta es: "+resta);
+            case 2:
+                System.out.println("El resultado de la resta es: " + calcul.resta(num1, num2));
                 break;
-                
-            case 3 : 
-                System.out.println("el resultado de la multiplicacion es : "+multiplicacion);
-              break;
-              
-            case 4 : 
-                System.out.println("el resultado de la division es : "+division);
+            case 3:
+                System.out.println("El resultado de la multiplicación es: " + calcul.multiplicacion(num1,num2));
                 break;
-                
+            case 4:
+               
+                    System.out.println("El resultado de la división es: " +calcul.division(num1,num2));
+                break;
             default:
-                System.out.println("opcion no valida");
+                System.out.println("Opción no válida");
         }
         
+
+      
     }
-    
 }
